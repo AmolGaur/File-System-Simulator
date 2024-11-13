6399b08f405a9ad50e17d8639c8cb72e1a982a9e
AnshMehta1 <ansh.mehta522007@gmail.com>
2024-11-13 20:15:20 +0530

    setting up root and its memory allocation

--- file_system.c
+++ file_system.c
@@ -29,9 +29,15 @@ typedef struct {
     Directory *current_dir;  // current directory the user is in
 } FileSystem;
 
-// 
-
 int main() {
+    // Setting up file system with root directory
+    FileSystem fs;
+    fs.root = (Directory *)malloc(sizeof(Directory));
+    strncpy(fs.root->name, "/", MAX_NAME_LENGTH);
+    fs.root->parent = NULL;
+    fs.root->num_subdirs = 0;
+    fs.root->num_files = 0;
+    fs.current_dir = fs.root;
 
     return 0;
 }
