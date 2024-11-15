3c6681b9f2053ed2c374cff94c295368ce928e37
AnshMehta1 <ansh.mehta522007@gmail.com>
2024-11-15 16:20:14 +0530

    Added function headers, colors, read and write file definitions

--- file_system.c
+++ file_system.c
@@ -6,6 +6,12 @@
 #define MAX_FILES 100
 #define MAX_DIRS 100
 
+#define RESET_COLOR "\033[0m"
+#define SUCCESS_COLOR "\033[0;32m"
+#define ERROR_COLOR "\033[0;31m"
+#define PROMPT_COLOR "\033[0;36m"
+#define SAVE_FILE "filesystem_data.txt"
+
 // File structure
 typedef struct {
     char name[MAX_NAME_LENGTH];
@@ -28,6 +34,50 @@ typedef struct {
     Directory *root;         // root directory
     Directory *current_dir;  // current directory the user is in
 } FileSystem;
+
+// Function Headers
+void save_filesystem(FileSystem *fs);
+void load_filesystem(FileSystem *fs);
+void create_directory(FileSystem *fs, const char *dirname);
+void create_file(FileSystem *fs, const char *filename);
+void write_file(FileSystem *fs, const char *filename, const char *data);
+void print_tree(Directory *dir, int depth);
+void delete_file(FileSystem *fs, const char *filename);
+void read_file(FileSystem *fs, const char *filename);
+void change_directory(FileSystem *fs, const char *dirname);
+void list_directory(FileSystem *fs);
+void print_help();
+
+// Function to read a file
+void read_file(FileSystem *fs, const char *filename) {
+    Directory *current = fs->current_dir;
+    for (int i = 0; i < current->num_files; i++) {
+        if (strcmp(current->files[i]->name, filename) == 0) {
+            if (current->files[i]->data != NULL) {
+                printf("Reading file %s:\n%s\n", filename, current->files[i]->data);
+            } else {
+                printf("File is empty.\n");
+            }
+            return;
+        }
+    }
+    printf(ERROR_COLOR "File not found.\n" RESET_COLOR);
+}
+
+// Function to write data to a file
+void write_file(FileSystem *fs, const char *filename, const char *data) {
+    Directory *current = fs->current_dir;
+    for (int i = 0; i < current->num_files; i++) {
+        if (strcmp(current->files[i]->name, filename) == 0) {
+            current->files[i]->data = (char *)malloc(strlen(data) + 1);
+            strcpy(current->files[i]->data, data);
+            current->files[i]->size = strlen(data);
+            printf(SUCCESS_COLOR "Data written to %s.\n" RESET_COLOR, filename);
+            return;
+        }
+    }
+    printf(ERROR_COLOR "File not found.\n" RESET_COLOR);
+}
 
 // Main function to initialize file system and start CLI
 int main() {
